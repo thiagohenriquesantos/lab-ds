635a1c8d8c6789d1572260d353641a57522790c5
Thiago Santos <thiagosantosdev@gmail.com>
2022-10-15 18:52:36 -0300

    tokens criados

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,24 @@ module.exports = {
     './src/**/*.tsx',
   ],
   theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32
+    },
+    color: {
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-400': '#7C7C8A',
+      'gray-200': '#c4c4cc',
+      'gray-100': '#e1e1e6',
+
+      'cyan-500': '#81D8F7',
+      'cyan-300': '#9BE1FB',
+    },
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif',
