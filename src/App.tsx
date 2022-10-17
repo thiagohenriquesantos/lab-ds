50a06d503c2e802130af1635b4c32e968d52492d
Thiago Santos <thiagosantosdev@gmail.com>
2022-10-17 18:27:49 -0300

    adicionado logo no template

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import './styles/global.css';
+import { Logo } from './Logo'
 
 export function App() {
   return (
-    <>
-      <h1 className='font-bold text-2xl text-violet-500'>Hello World</h1>
-
-      <button className='bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300'>
-        Enviar
-      </button>
-    </>
+    <div className="w-screen h-screen bg-gray-900 flex items-center justify-center text-gray-100">
+      <Logo />
+    </div>
   )
 };
