59e06d15d25f7f619a7cd818eb5eed3f7f57c90b
Thiago Santos <thiagosantosdev@gmail.com>
2022-10-17 21:33:11 -0300

    adicionado cenario de teste no storybook

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,4 +1,5 @@
 import { Envelope, Lock } from 'phosphor-react';
+import { FormEvent, useState } from 'react';
 import { Button } from '../components/Button';
 import { Checkbox } from '../components/Checkbox';
 import { Heading } from '../components/Heading';
@@ -7,6 +8,13 @@ import { TextInput } from '../components/TextInput';
 import { Logo } from '../Logo';
 
 export function SignIn() {
+  const [isUserSignedIn, setIsUserSignedIn] = useState(false);
+
+  function handleSignIn(event: FormEvent) {
+    event.preventDefault();
+
+    setIsUserSignedIn(true);
+  }
   return (
     <div className="w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-100">
       <header className="flex flex-col items-center">
@@ -21,7 +29,8 @@ export function SignIn() {
         </Text>
       </header>
 
-      <form className="flex flex-col gap-4 items-stretch w-full max-w-sm mt-10">
+      <form onSubmit={handleSignIn} className="flex flex-col gap-4 items-stretch w-full max-w-sm mt-10">
+        {isUserSignedIn && <Text>Login realizado!</Text>}
         <label htmlFor="email" className="flex flex-col gap-3">
           <Text className="font-semibold">Endereço de e-mail</Text>
           <TextInput.Root>
